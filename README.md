2e37f5de4b4db1dfc613167ae7612fd1c6a3407c
Peter <peterluong1123@gmail.com>
2023-01-22 22:10:42 -0700

    MERN-stackInfrastructure

---
+++ README.md
@@ -0,0 +1,3 @@
+#MERN-Stack infrastructure
+
+Clone this repo to provide the start code for a comprehensive MERN-stack project including token-based authentication
---
+++ controllers/api/users.js
@@ -0,0 +1,50 @@
+const jwt = require('jsonwebtoken');
+const User = require('../../models/user');
+const bcrypt = require('bcrypt');
+
+module.exports = {
+  create,
+  login,
+  checkToken
+};
+
+async function login(req, res) {
+  try {
+    const user = await User.findOne({email: req.body.email});
+    if (!user) throw new Error();
+    const match = await bcrypt.compare(req.body.password, user.password);
+    if (!match) throw new Error();
+    const token = createJWT(user);
+    res.json(token);
+  } catch (err) {
+    res.status(400).json('Bad Credentials');
+  }
+}
+
+async function create(req, res) {
+  try {
+    const user = await User.create(req.body);
+    const token = createJWT(user);
+    // The token is a string, but yes, we can
+    // res.json a string
+    res.json(token);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+}
+
+function checkToken(req, res) {
+  // req.user will always be there for you when a token is sent
+  console.log('req.user', req.user);
+  res.json(req.exp);
+}
+
+/*-- Helper Functions --*/
+function createJWT(user) {
+  return jwt.sign(
+    // extra data for the payload
+    { user },
+    process.env.SECRET,
+    { expiresIn: '24h' }
+  );
+}
